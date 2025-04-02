6b89849c4cd4635b378b5142e99b01bf1076365e
Luiz Conde <luizconde.dev@outlook.com>
2025-04-02 00:14:54 -0300

    Creating Initial Structure of Project

---
+++ src/components/ImcCalculator/index.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+
+import styles from './ImcCalculator.module.css';
+
+const ImcCalculator = () => {
+    return(
+        <h1>Oii</h1>
+    )
+}
+
+export default ImcCalculator;
